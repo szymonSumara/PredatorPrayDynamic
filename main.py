969eb650c0ce6c85541538b12f14fdd6d32082c7
kkoz3434 <jkozlak@student.agh.edu.pl>
2021-06-05 16:44:32 +0200

    3/3 of equations

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def lotka_volterra1(x0, y0, h, end, a, b, c, d, e=0, f=0):
     :return:
     '''
 
-
     def fun_x(x, y):
         return (a - b * y) * x - e * x
 
@@ -49,13 +48,65 @@ def lotka_volterra1(x0, y0, h, end, a, b, c, d, e=0, f=0):
     mpl.ylabel("population")
     mpl.legend()
     mpl.show()
+    mpl.plot(x_val, y_val)
+    mpl.show()
+
+    return [t_arg, x_val, y_val]
 
 
-def lotka_volterra2(x0, y0, h, end):
+def lotka_volterra2(x0, y0, h, end, K1, K2, a, b, r1, r2):
+    '''
 
+    :param x0:  Preys amount
+    :param y0: Predator amount
+    :param h: step
+    :param end: endpoint
+    :param K1: environment capacity for prey
+    :param K2: environment capacity for predator
+    :param a: competition factor (prey)
+    :param b: competition factor (predator)
+    :param r1: growth of preys
+    :param r2: growth of predators
+    :return:
+    '''
 
+    def fun_x(x, y):
+        return r1*x*((K1-x-a*y)/ K1)
 
+    def fun_y(x, y):
+        return r2*y*((K2-y-b*x)/K2)
+
+    t_arg = []
+    x_val = []
+    y_val = []
+
+    t_curr = 0
+    x_curr = x0
+    y_curr = y0
+    while t_curr < end:
+        y_prev = y_curr
+        y_curr = y_prev + h * fun_y(x_curr, y_curr)
+
+        x_prev = x_curr
+        x_curr = x_prev + h * fun_x(x_curr, y_curr)
+
+        y_val.append(y_curr)
+        x_val.append(x_curr)
+        t_arg.append(t_curr)
+
+        t_curr += h
+
+    mpl.plot(t_arg, x_val, label="preys")
+    mpl.plot(t_arg, y_val, label="predators")
+    mpl.xlabel("time")
+    mpl.ylabel("population")
+    mpl.legend()
+    mpl.show()
+    mpl.plot(x_val, y_val)
+    mpl.show()
+
+    return [t_arg, x_val, y_val]
 
 
 if __name__ == '__main__':
-    lotka_volterra1(1,0.5, 0.001, 10, 1, 1, 0.8, 0.8)
+    lotka_volterra2(10,10, 0.01, 20, 50, 20, 1.2, 1.0, 1.1, 1.05)
